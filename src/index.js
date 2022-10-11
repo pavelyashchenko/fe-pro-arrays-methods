80a20a076fe035b0ac27c24ec6223ea360975d5b
Pavel Yashchenko <yashchenkops@gmail.com>
2022-10-11 22:10:05 +0300

    home work fe-pro-arrays-methods

--- src/index.js
+++ src/index.js
@@ -1,59 +1,61 @@
 import { goods } from './goods.js';
 
+const filterFunc = (key, value) => goods.filter(product => product[key] === value);
+
 /**
  * @param {string} brand
  * @returns {*}
  */
-const brandFilter = (brand) => {};
+const brandFilter = (brand) => filterFunc('brand', brand);
 
 /**
  * @param {string} color
  * @returns {*}
  */
-const colorFilter = (color) => {};
+const colorFilter = (color) => filterFunc('color', color);
 
 /**
  * @param {string} model
  * @returns {*}
  */
-const modelFilter = (model) => {};
+const modelFilter = (model) => filterFunc('model', model);
 
 /**
  * @param {number} memory
  * @returns {*}
  */
-const memoryFilter = (memory) => {};
+const memoryFilter = (memory) => filterFunc('memory', memory);
 
 /**
  * @param {number} price
  * @returns {*}
  */
-const priceFilter = (price) => {};
+const priceFilter = (price) => filterFunc('price', price);
 
 /**
  * @param {string} country
  * @returns {*}
  */
-const countryFilter = (country) => {};
+const countryFilter = (country) => filterFunc('country', country);
 
 /**
  * @param {string} os
  * @returns {*}
  */
-const osFilter = (os) => {};
+const osFilter = (os) => filterFunc('os', os);
 
 /**
  * @param {number} from
  * @param {number} to
  */
-const rangeFilter = (from, to) => {};
+const rangeFilter = (from, to) => goods.filter(product => product.price >= from && product.price <= to);
 
-const minPriceReducer = () => {};
+const minPriceReducer = () => goods.reduce((minPrice, product) => product.price < minPrice ? product.price : minPrice, goods[0].price);
 
-const maxPriceReducer = () => {};
+const maxPriceReducer = () => goods.reduce((minPrice, product) => product.price > minPrice ? product.price : minPrice, goods[0].price);
 
-const toMaxSorter = () => {};
-const toMinSorter = () => {};
+const toMaxSorter = () => goods.sort((a, b) => b.price - a.price);
+const toMinSorter = () => goods.sort((a, b) => a.price - b.price);
 
 export const filters = {
   brandFilter,
